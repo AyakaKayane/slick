133d17d6acad6147e71ccb9d8c3323b9fdad66f9
EC2 Default User <ec2-user@ip-172-31-32-39.ap-northeast-1.compute.internal>
2021-03-14 09:05:46 +0000

    slick

---
+++ main.js
@@ -0,0 +1,9 @@
+$('.slick01').slick({
+  autoplay: true,　　　　//自動再生
+  dots: true,            //すらいど下のドットナビ
+  autoplayspeed: 4000,   //４秒間隔で自動再生
+  pauseOnHover: false,   //ホバーしたときにスライドを一時停止しない　
+  prevArrow: '<button class="slide-arrow prev-arrow"></button>',
+  nextArrow: '<button class="slide-arrow next-arrow"></button>',
+});
+
